d6cf09a57b9dd327cf65d4ea0a85eb462c2f2bf7
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-03-27 13:40:53 +0300

    [init] run mv to start a new project

---
+++ src/main/java/com/tempconvert/App.java
@@ -0,0 +1,13 @@
+package com.tempconvert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
